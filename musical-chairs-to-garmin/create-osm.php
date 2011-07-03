674faaf6d74f9f2c86f0d14694a7f35082762491
Derick Rethans <github@derickrethans.nl>
2011-07-03 15:22:51 +0100

    Updated script for new file format, and some new checks that are now possible.

--- musical-chairs-to-garmin/create-osm.php
+++ musical-chairs-to-garmin/create-osm.php
@@ -9,27 +9,56 @@ $f = fopen("compress.zlib://matches-latest.csv.gz", 'r');
 
 while( false !== ($line = fgetcsv($f) ) )
 {
-	if ( $line[1] == '' )
+	if ( $line[1] == '' && $line[2] == '' )
 	{
 		continue;
 	}
-	if ( strlen( trim( $line[6] ) ) > 0 && $line[7] == 0 )
+	if ( ( strlen( trim( $line[8] ) ) > 0 || strlen( trim( $line[9] ) ) > 0 ) && $line[5] == 0 )
 	{
 		continue;
 	}
-	$osm->openNode( $line[3], $line[2] );
-	if ( strlen( trim( $line[6] ) ) == 0 )
+	if ( strlen( trim( $line[8] ) ) == 0 && strlen( trim( $line[9] ) ) == 0 )
 	{
+		$osm->openNode( $line[4], $line[3] );
+		$missing = strlen( $line[1] ) ? $line[1] : $line[2];
 		$osm->writeTag( 'osmanalysis', 'missing' );
-		$osm->writeTag( "name", htmlspecialchars( "Missing: {$line[1]}", ENT_QUOTES ) );
+		$osm->writeTag( "name", htmlspecialchars( "Missing: {$missing}", ENT_QUOTES ) );
+		$osm->closeNode();
+		continue;
 	}
-	else
+	else if ( strlen( trim( $line[2] ) ) != 0 && strlen( trim( $line[9] ) ) == 0 )
 	{
-		$osm->writeTag( 'osmanalysis', 'mismatch' );
-		$osm->writeTag( 'osmanalysislevel', $line[7] );
-		$osm->writeTag( "name", htmlspecialchars( "No match: {$line[1]} -> {$line[6]}", ENT_QUOTES ) );
+		$osm->openNode( $line[4], $line[3] );
+		$osm->writeTag( 'osmanalysis', 'missing' );
+		$osm->writeTag( "name", htmlspecialchars( "Missing ref: {$line[2]}", ENT_QUOTES ) );
+		$osm->closeNode();
+		continue;
 	}
-	$osm->closeNode();
+	else if ( strlen( trim( $line[1] ) ) != 0 && strlen( trim( $line[8] ) ) == 0 )
+	{
+		$osm->openNode( $line[4], $line[3] );
+		$osm->writeTag( 'osmanalysis', 'missing' );
+		$osm->writeTag( "name", htmlspecialchars( "Missing name: {$line[1]}", ENT_QUOTES ) );
+		$osm->closeNode();
+		continue;
+	}
+	if ( strlen( trim( $line[2] ) ) != 0 && ( $line[2] != $line[9] ) )
+	{
+		$osm->openNode( $line[4], $line[3] );
+		$osm->writeTag( 'osmanalysis', 'missing' );
+		$osm->writeTag( "name", htmlspecialchars( "Diff ref: {$line[2]} -> {$line[9]}", ENT_QUOTES ) );
+		$osm->closeNode();
+	}
+
+	{
+		$osm->openNode( $line[4], $line[3] );
+		$expected = strlen( $line[1] ) ? $line[1] : $line[2];
+		$real     = strlen( $line[8] ) ? $line[8] : $line[9];
+		$osm->writeTag( 'osmanalysis', 'mismatch' );
+		$osm->writeTag( 'osmanalysislevel', $line[5] );
+		$osm->writeTag( "name", htmlspecialchars( "Diff: {$expected} -> {$real}", ENT_QUOTES ) );
+		$osm->closeNode();
+	}
 }
 
 $osm->closeOsm();
