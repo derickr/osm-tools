b55e945300bcc6098aa9d5645999a61e5e401663
Derick Rethans <github@derickrethans.nl>
2011-07-07 09:44:33 +0100

    Update path that has changed on the data dump server.

--- musical-chairs-to-garmin/run.sh
+++ musical-chairs-to-garmin/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O matches-latest.csv.gz http://ris.dev.openstreetmap.org/oslmusicalchairs/matchdumps/matches-latest.csv.gz
+wget -O matches-latest.csv.gz http://ris.dev.openstreetmap.org/oslmusicalchairs/data/matchdumps/matches-latest.csv.gz
 php create-osm.php
 ./convert.sh
 cp images/gmapsupp.img .
